87fc28f5a9172951753f378e274ea194f5134d4b
avarugh1 <avarugh1@users.noreply.github.com>
2021-01-18 16:42:28 -0500

    test with simple commands

---
+++ index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(){
+    const inputs = {
+        token: core.getInput('repo-token', {required: true}),
+    }
+
+    const ctx = github.context;
+    const base = ctx.payload.before;
+    const head = ctx.payload.after;
+
+    core.setOutput('test finished!');
+}
