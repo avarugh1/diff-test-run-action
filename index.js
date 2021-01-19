db54fa86b72ac4c5c3f1f4e9c4329622a923ac91
avarugh1 <avarugh1@users.noreply.github.com>
2021-01-18 23:26:43 -0500

    m

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ const { execSync } = require('child_process');
 
 // figure out ncc error with index.js collision
 /*const createTestCafe = require('testcafe');
-
-
-function parseResults(reportName){
-    let data = JSON.parse(fs.readFileSync(reportName));
-    console.log(data);
-}
-
 async function runTests(filesFiltered){
     let testArr = [];
     Object.values(filesFiltered).forEach(ele => {
@@ -35,9 +28,56 @@ async function runTests(filesFiltered){
     }
 }*/
 
-function parseResults(reportName){
+let message = '';
+let prNum = 0;
+
+async function parseResults(reportName){
     let data = JSON.parse(fs.readFileSync(reportName));
     console.log(data);
+
+    let errAndUnstable = [];
+    let unstableOnly = [];
+    let errOnly = [];
+
+    data.fixtures.forEach(fixture => {
+        fixture.tests.forEach(test => {
+            if((test.errs.length > 0) && (test.unstable)){
+                errAndUnstable.push({
+                    name: test.name,
+                    path: fixture.path,
+                    errs: test.errs,
+                    unstable: true
+                });
+            }else if(test.errs.length > 0){
+                errOnly.push({
+                    name: test.name,
+                    path: fixture.path,
+                    errs: test.errs
+                });
+            }else if(test.unstable){
+                unstableOnly.push({
+                    name: test.name,
+                    path: fixture.path,
+                    unstable: true
+                });
+            }
+        });
+    });
+
+    if(errAndUnstable.length + unstableOnly.length + errOnly.length){
+        message += 'Great news: no tests were unstable or had errors!'
+    }else{
+        message += 'Uh oh, some tests had errors or were unstable!\n\n'
+        message += '| Path | Name |\n';
+        let outputArr = errAndUnstable.concat(unstableOnly).concat(errOnly);
+        outputArr.forEach(ele => message += ('| ' + ele.path + ' | ' + ele.name + ' | \n'));  
+    }
+
+    const response = await octokit.issues.createComment({
+        ...github.context.repo,
+        issue_number: prNum,
+        body: message
+    });
 }
 
 async function runTests(filesFiltered){
@@ -47,17 +87,26 @@ async function runTests(filesFiltered){
             testArr.push(ele2.filename);
         })
     });
-    
-    // dirty workaround from https://github.com/DevExpress/testcafe-action/blob/master/index.js
-    // due to index.js collision 
-    let testcafeCmd = 'npx testcafe chrome:headless ' + testArr.join(' ') + 
-                        ' -q ' + '-r json:github-action-report.json';
 
-    execSync(`npm i testcafe`);
-    execSync(`${testcafeCmd}`, { stdio: 'inherit' });
-    console.log('done with npm commands');
-    parseResults('github-action-report.json');
-    console.log('done with parse commands');
+    if(testArr.length > 0) {
+        message += 'Hello! Friendly test checker here! Looks like some test file changes were made.\n'
+        message += 'I\'ll help you out by checking their stability! Good luck!\n\n'
+        message += 'File paths being checked are below:\n'
+        testArr.forEach(ele => message += ('1. ' + ele + '\n'));
+        message += '\n'
+    
+    
+        // dirty workaround from https://github.com/DevExpress/testcafe-action/blob/master/index.js
+        // due to index.js collision 
+        let testcafeCmd = 'npx testcafe chrome:headless ' + testArr.join(' ') + 
+                            ' -q ' + '-r json:github-action-report.json';
+
+        execSync(`npm i testcafe`);
+        execSync(`${testcafeCmd}`, { stdio: 'inherit' });
+        console.log('done with npm commands');
+        parseResults('github-action-report.json');
+        console.log('done with report parse commands');
+    }
 }
 
 function filterEligibleFiles(ele){
@@ -70,23 +119,21 @@ function filterEligibleFiles(ele){
     return (isInDir && fileExt);
 }
 
-async function checkValidFiles(octokit, prNum, filesFiltered){
-    let message = '';
-
+async function checkValidFiles(octokit, filesFiltered){
     // for all the original added, removed, modified, renamed arrays
     Object.keys(filesFiltered).forEach(ele => {
         // Only use the eligible test files based on our file filter criteria
         filesFiltered[ele] = filesFiltered[ele].filter(filterEligibleFiles);
 
         // display files which will run to the user
-        if(filesFiltered[ele].length > 0){
+        /*if(filesFiltered[ele].length > 0){
             message += (ele.charAt(0).toUpperCase() + ele.slice(1) + " Files are:\n");
 
             filesFiltered[ele].forEach(ele2 => {
                 message += (ele2.filename + '\n');
             });
             message += '\n';
-        }
+        }*/
     });
 
     /*const response = await octokit.issues.createComment({
@@ -103,7 +150,8 @@ async function run(){
         }
 
         const pr = github.context.payload.pull_request;
-        const [ base, head, prNum ] = [ pr.base, pr.head, pr.number ];
+        prNum = pr.number;
+        const [ base, head ] = [ pr.base, pr.head ];
         const message = 'Base is: ' + base.ref + '\nHead is: ' + head.ref;
 
         const octokit = github.getOctokit(inputs.token);
@@ -123,7 +171,7 @@ async function run(){
             filesFiltered[ele.status].push(ele);
         });
 
-        checkValidFiles(octokit, prNum, filesFiltered);
+        checkValidFiles(octokit, filesFiltered);
         runTests(filesFiltered);
         /*const response = await octokit.issues.createComment({
             ...github.context.repo,
